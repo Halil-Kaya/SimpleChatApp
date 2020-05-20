4dc33d252e19242370a1b8bfada331bf55930dc8
Halil-Kaya <hibrahimkayahik@gmail.com>
2020-05-20 07:52:21 +0300

    HesapAyarlari ekranı eklendi tasarımı yapıldı

--- app/src/main/java/com/halilkaya/loginandregisterapp/HesapAyarlari.kt
+++ app/src/main/java/com/halilkaya/loginandregisterapp/HesapAyarlari.kt
@@ -2,11 +2,20 @@ package com.halilkaya.loginandregisterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_hesap_ayarlari.*
 
 class HesapAyarlari : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hesap_ayarlari)
+
+        tvMailSifreGuncelle.setOnClickListener {
+
+            gizliConstrainLayout.visibility = View.VISIBLE
+
+        }
+
     }
 }
